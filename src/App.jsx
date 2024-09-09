c55aa2106b9898afc5c8e40e02810582c08bfe1d
Bitar Youssef <bitardev@gmail.com>
2024-09-09 12:43:15 +0100

    Add Google Analytics support for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 import Sidebar from "./components/SideBar/Sidebar";
 import Main from "./components/Main/Main";
-
+import { Analytics } from "@vercel/analytics/react"
 const App = () => {
   return (
     <>
       <Sidebar />
       <Main />
+      <Analytics />
     </>
   );
 };
